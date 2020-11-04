0ac2941ff248c65c9404f56e77a045b91766f94b
Krzysztof Pawlak <krzysztofpawlak2000@gmail.com>
2020-11-04 18:28:20 +0100

    add stars animation

--- script/backgroundScript.js
+++ script/backgroundScript.js
@@ -137,5 +137,19 @@ const backgroundAnimation = function () {
         canvas.height = window.innerHeight;
         animate();
     };
+
+    var tester = document.getElementById('tester');
+var wrapper = document.getElementById('wrapper');
+
+window.onscroll = function() {
+  wrapper.style.backgroundColor = checkVisible(tester) ? '#4f4' : '#f44';
+};
+
+function checkVisible(elm) {
+  var rect = elm.getBoundingClientRect();
+  var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+  return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+}
+    
 }
 window.addEventListener('DOMContentLoaded', onDomReadyFunction);
